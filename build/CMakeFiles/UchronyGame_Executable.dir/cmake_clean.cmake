5102003bf0af8a754d88c0cb0ef2b28a830ccfa4
Rosa Rosales <rosamilib@gmail.com>
2025-11-11 02:41:18 -0400

    Backup current state before merge with development

---
+++ build/CMakeFiles/UchronyGame_Executable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UchronyGame_Executable.dir/src/main.cpp.o"
+  "CMakeFiles/UchronyGame_Executable.dir/src/main.cpp.o.d"
+  "UchronyGame_Executable"
+  "UchronyGame_Executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UchronyGame_Executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
