da84f95b554a7328a15c05d2448dffe14593f989
Edwer Soret <edwer2soret@gmail.com>
2025-11-22 16:33:06 -0400

    Agregado un gitignore, archivos base de game y scene

---
+++ build/CMakeFiles/UchronyGame_Executable.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/Entities/Inventory.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/Entities/Inventory.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/Entities/Inventory.cpp.o.d"
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/Entities/Item.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/Entities/Item.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/Entities/Item.cpp.o.d"
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/Entities/Player.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/Entities/Player.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/Entities/Player.cpp.o.d"
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/Utils/Collision.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/Utils/Collision.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/Utils/Collision.cpp.o.d"
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/Utils/NavGrid.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/Utils/NavGrid.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/Utils/NavGrid.cpp.o.d"
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/Utils/Pathfinder.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/Utils/Pathfinder.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/Utils/Pathfinder.cpp.o.d"
+  "/home/edw/Escritorio/pr/Uchrony-Game/src/demo.cpp" "CMakeFiles/UchronyGame_Executable.dir/src/demo.cpp.o" "gcc" "CMakeFiles/UchronyGame_Executable.dir/src/demo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
