a9a1a4ae269269e82fd61c984c73450adb3cbbd9
Edwer Soret <edwer2soret@gmail.com>
2025-11-23 02:34:20 -0400

    Agg apuntador a game en la clase GameState

--- Include/Core/GameState.hpp
+++ Include/Core/GameState.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+class Game;
+
 class GameState {
+protected:
+    Game* game; //El numero de mi jefe
+
 public:
     virtual ~GameState(){}
+
+    void setGame(Game* gamePtr) {
+        this->game = gamePtr;
+    }
 
     virtual void init() = 0;
 
